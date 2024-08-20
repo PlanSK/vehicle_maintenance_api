86314c58c4dff752642925c46de19c92596bef15
PlanSK <skapkanshchikov@gmail.com>
2024-08-20 20:50:35 +0300

    Add type hinting for async functions.

--- core/database.py
+++ core/database.py
@@ -26,12 +26,14 @@ class DatabaseInterface:
         )
         return session
 
-    async def session_dependency(self):
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
             yield session
             await session.close()
 
-    async def scoped_session_dependency(self):
+    async def scoped_session_dependency(
+        self,
+    ) -> AsyncGenerator[AsyncSession, None]:
         session = self.get_scoped_session()
         yield session
         await session.close()
