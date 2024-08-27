43ec91e93f6ec2f6dd62ab2a597cf4f8fa3bcec3
PlanSK <skapkanshchikov@gmail.com>
2024-08-27 21:15:50 +0300

    Add VIN code type and VIN code validator with calculating checksum.

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,9 @@
 __all__ = (
     "DatabaseInterface",
     "db_interface",
+    "vin_code_validator",
+    "VIN_Type"
 )
 
 from .database import DatabaseInterface, db_interface
+from .vin import VIN_Type, vin_code_validator
