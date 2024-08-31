c0b66859589fbb1435b7edbd2c499989d6e485ea
PlanSK <skapkanshchikov@gmail.com>
2024-08-31 14:21:34 +0300

    Disabled base auth views. Add logger for HTTP exceptions in auth jwt.

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
-from .auth.auth_jwt import router as jwt_router
-from .auth.views import router as auth_router
+from .auth.views import router as jwt_router
 from .users.views import router as users_router
 
-auth_router.include_router(jwt_router)
 router = APIRouter()
 router.include_router(router=users_router, prefix="/users")
-router.include_router(router=auth_router)
+router.include_router(router=jwt_router)
--- api_v1/auth/views.py
+++ api_v1/auth/views.py
@@ -1,134 +1,101 @@
-import datetime
-import secrets
-import uuid
-from typing import Annotated
+from fastapi import APIRouter, Depends, Form, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+from loguru import logger
+from pydantic import BaseModel
 
-from fastapi import (
-    APIRouter,
-    Cookie,
-    Depends,
-    Header,
-    HTTPException,
-    Response,
-    status,
+from auth.password_operators import password_validation
+from auth.utils import decode_jwt, encode_jwt
+from core.config import settings
+
+from .schemas import UserSchema, users_db
+
+LOGIN_ROUTER_PREFIX = "/login/"
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str
+
+
+router = APIRouter(prefix="/jwt", tags=["JWT Auth"])
+oauth_token = OAuth2PasswordBearer(
+    tokenUrl=settings.api_v1_prefix + router.prefix + LOGIN_ROUTER_PREFIX
 )
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-
-router = APIRouter(prefix="/auth", tags=["Auth"])
-
-security = HTTPBasic()
 
 
-users_dict = {"admin": "admin"}
+http_forbidden_exception: HTTPException = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN, detail="Access forbidden."
+)
+http_unauth_exception: HTTPException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Incorrect auth data.",
+)
 
 
-@router.get("/basic-auth/")
-def basic_auth_credentials(
-    credencials: Annotated[HTTPBasicCredentials, Depends(security)]
+def auth_user_validate(username: str = Form(), password: str = Form()):
+    if not (user := users_db.get(username)):
+        logger.error(f"User {username!r} not found in db.")
+        raise http_unauth_exception
+
+    if not password_validation(password=password, hash=user.password):
+        logger.error(f"User {username!r} password incorrect.")
+        raise http_unauth_exception
+    elif not user.is_active:
+        logger.error(f"Inactive user {username!r} try to login.")
+        raise http_forbidden_exception
+
+    return user
+
+
+def get_current_payload_from_token(
+    token: str = Depends(oauth_token),
+) -> UserSchema:
+    try:
+        payload = decode_jwt(token=token)
+    except InvalidTokenError:
+        logger.error("Decode token error: Invalid token error.")
+        raise http_unauth_exception
+    return payload
+
+
+def get_active_user_from_payload(
+    payload: dict = Depends(get_current_payload_from_token),
+) -> UserSchema:
+    username: str | None = payload.get("username")
+    if username and (user := users_db.get(username)):
+        return user
+    logger.error(f"User {username!r} not found in db.")
+    raise http_unauth_exception
+
+
+def get_current_active_user(
+    user: UserSchema = Depends(get_active_user_from_payload),
+):
+    if user.is_active:
+        return user
+    logger.error(f"Inactive user {user.username!r} try to login.")
+    raise http_forbidden_exception
+
+
+@router.post(LOGIN_ROUTER_PREFIX)
+def auth_user_jwt(user: UserSchema = Depends(auth_user_validate)):
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email,
+    }
+    token = encode_jwt(jwt_payload)
+    return TokenInfo(access_token=token, token_type="Bearer")
+
+
+@router.get("/users/me/")
+def auth_user_get_self_info(
+    payload: dict = Depends(get_current_payload_from_token),
+    user: UserSchema = Depends(get_current_active_user),
 ):
     return {
-        "message": "Hi!",
-        "username": credencials.username,
-        "password": credencials.password,
+        "username": user.username,
+        "email": user.email,
+        "logged in": payload.get("iat"),
     }
-
-
-def get_auth_user_username(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
-) -> str:
-    unauth_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid username or password.",
-        headers={"WWW-Authenticate": "Basic"},
-    )
-    correct_password = users_dict.get(credentials.username)
-    if correct_password is None:
-        raise unauth_exception
-
-    if not secrets.compare_digest(credentials.password, correct_password):
-        raise unauth_exception
-    return credentials.username
-
-
-@router.get("/basic-auth-username/")
-def basic_auth_username(auth_username: str = Depends(get_auth_user_username)):
-    return {
-        "message": f"Hi, {auth_username}",
-        "username": auth_username,
-    }
-
-
-static_auth_tokens = {
-    "ffcb46aac55299e193cdef5bab3d7c0f": "admin",
-    "43454e2365d55563905749648a76eb9d": "john",
-}
-
-
-def get_username_by_static_auth_token(
-    static_token: str = Header(alias="x-static-auth-token"),
-) -> str:
-    if token := static_auth_tokens.get(static_token):
-        return token
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token."
-    )
-
-
-@router.get("/some-http-header-auth/")
-def some_http_header_auth(
-    header_username: str = Depends(get_username_by_static_auth_token),
-):
-    return {
-        "message": f"Hi, {header_username}",
-        "username": header_username,
-    }
-
-
-COOKIES: dict = {}
-COOKIE_SESSION_ID_KEY = "web-app-session-id"
-
-
-def generate_session_id() -> str:
-    return uuid.uuid4().hex
-
-
-@router.get("/login-cookie/")
-def auth_login_set_cookie(
-    response: Response, auth_username: str = Depends(get_auth_user_username)
-):
-    session_id = generate_session_id()
-    COOKIES[session_id] = {
-        "username": auth_username,
-        "login_at": int(datetime.datetime.now().timestamp()),
-    }
-    response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
-    return {"result": "ok"}
-
-
-def get_session_data(session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY)):
-    if session_id not in COOKIES:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated.",
-        )
-    return COOKIES[session_id]
-
-
-@router.get("/check-cookie/")
-def auth_login_check_cookie(
-    user_session_data: dict = Depends(get_session_data),
-):
-    username = user_session_data.get("username")
-    return {"message": f"Hello, {username}!", **user_session_data}
-
-
-@router.get("/logout-cookie/")
-def auth_logout_cookie(
-    response: Response,
-    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
-    user_session_data: dict = Depends(get_session_data),
-):
-    COOKIES.pop(session_id)
-    response.delete_cookie(COOKIE_SESSION_ID_KEY)
-    username = user_session_data.get("username")
-    return {"message": f"Bye bye, {username}!", **user_session_data}
