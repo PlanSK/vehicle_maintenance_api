2afc1c462ad1d7e5d16f8c66b2283bee0d166c5d
PlanSK <skapkanshchikov@gmail.com>
2024-09-06 22:07:21 +0300

    Add crud for Events.

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .auth.views import router as jwt_router
+from .events.views import router as event_router
 from .users.views import router as users_router
 from .vehicles.views import router as vehicle_router
 from .works.views import router as works_router
@@ -10,3 +11,4 @@ router.include_router(router=users_router, prefix="/users")
 router.include_router(router=jwt_router)
 router.include_router(router=vehicle_router)
 router.include_router(router=works_router)
+router.include_router(router=event_router)
---
+++ api_v1/events/crud.py
@@ -0,0 +1,113 @@
+from sqlalchemy import Result, desc, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.events import Event, MileageEvent
+from core.schemas.events import Event as EventSchema
+from core.schemas.events import EventCreate, EventUpdate
+from core.schemas.events import MileageEvent as MileageEventSchema
+from core.schemas.events import MileageEventCreate, MileageEventUpdate
+
+
+async def _get_objects_list_from_statement(
+    model, vehicle_id: int, session: AsyncSession, order_by_argument: str
+) -> list:
+    statement = (
+        select(model)
+        .where(model.vehicle_id == vehicle_id)
+        .order_by(desc(getattr(model, order_by_argument)))
+    )
+    result: Result = await session.execute(statement)
+    events_list: list = list(result.scalars().all())
+    return events_list
+
+
+async def _add_element_to_db(element, session: AsyncSession) -> None:
+    session.add(element)
+    await session.commit()
+    await session.refresh(element)
+
+
+async def get_event_by_id(
+    event_id: int, session: AsyncSession
+) -> Event | None:
+    return await session.get(Event, event_id)
+
+
+async def get_mileage_event_by_id(
+    event_id: int, session: AsyncSession
+) -> MileageEvent | None:
+    return await session.get(MileageEvent, event_id)
+
+
+async def create_event(
+    session: AsyncSession, event_data: EventCreate
+) -> Event:
+    event = Event(**event_data.model_dump())
+    await _add_element_to_db(element=event, session=session)
+    return event
+
+
+async def get_vehicle_events(
+    vehicle_id: int, session: AsyncSession
+) -> list[Event]:
+    events_list = await _get_objects_list_from_statement(
+        model=Event,
+        vehicle_id=vehicle_id,
+        session=session,
+        order_by_argument="work_date",
+    )
+    return events_list
+
+
+async def create_mileage_event(
+    session: AsyncSession, mileage_event_data: MileageEventCreate
+) -> MileageEvent:
+    mileage_event = MileageEvent(**mileage_event_data.model_dump())
+    await _add_element_to_db(element=mileage_event, session=session)
+    return mileage_event
+
+
+async def get_vehicle_mileage_events(
+    vehicle_id: int, session: AsyncSession
+) -> list[MileageEvent]:
+    events_list = await _get_objects_list_from_statement(
+        model=MileageEvent,
+        vehicle_id=vehicle_id,
+        session=session,
+        order_by_argument="mileage_date",
+    )
+    return events_list
+
+
+async def update_event(
+    session: AsyncSession, event: EventSchema, event_update: EventUpdate
+) -> EventSchema:
+    for name, value in event_update.model_dump(exclude_unset=True).items():
+        setattr(event, name, value)
+    await session.commit()
+    return event
+
+
+async def update_mileage_event(
+    session: AsyncSession,
+    mileage_event: MileageEventSchema,
+    mileage_event_update: MileageEventUpdate,
+) -> MileageEventSchema:
+    for name, value in mileage_event_update.model_dump(
+        exclude_unset=True
+    ).items():
+        setattr(mileage_event, name, value)
+    await session.commit()
+    return mileage_event
+
+
+async def delete_event(session: AsyncSession, event: EventSchema) -> None:
+    await session.delete(event)
+    await session.commit()
+
+
+async def delete_mileage_event(
+    session: AsyncSession, event: MileageEventSchema
+) -> None:
+    await session.delete(event)
+    await session.commit()
